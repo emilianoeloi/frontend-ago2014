2f63f3e85d11676af47cce1d752aa981955b8e18
unknown <aluno@JPLAB217.una.edu.br>
2014-09-27 15:48:14 -0300

    Aula 5

--- alunos/caeduardosilva/server/app.js
+++ alunos/caeduardosilva/server/app.js
@@ -1,10 +1,29 @@
-//console.log('Teste NodeJS')
+console.log('Aplicacao iniciada...')
 //node app.js
 
 var express = require('express');
 var app = express();
 
 app.use (express.static('C:/Users/aluno/Desktop/curso-front-end/alunos/caeduardosilva'));
+
+//back-end
+app.post('/api/contatos', function(req, res){
+	res.json(200, "Contato cadastrado com sucesso")
+})
+
+app.get('/api/contatos', function(req, res){
+	res.json(200, ["contato1", "contato2"])
+})
+
+app.put('/api/contatos', function(req, res){
+	res.json(200, "Contato atualizado com sucesso")
+})
+
+app.delete('/api/contatos', function(req, res){
+	res.json(200, "Contato deletado com sucesso")
+})
+
+
 
 /*
 app.get('/', function(req, res){
